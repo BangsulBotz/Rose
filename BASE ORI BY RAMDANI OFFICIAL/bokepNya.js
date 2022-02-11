c4c029b30585e6957347d7a763666e1348c40e51
BangsulBotz <bangsulstart1@gmail.com>
2022-02-11 18:23:42 +0700

    Monc

---
+++ BASE ORI BY RAMDANI OFFICIAL/bokepNya.js
@@ -0,0 +1,7 @@
+//ngapain di buka?
+//subrek Ramdani official lah tod di mana hati kalian ini:)
+//dah capek capem coding demi kalian
+//eh ujung2 nya gak di subrek:)
+
+// NOTES = SC INI BIKINAN GW SENDIRI LU JAN NGAKU NGAKU ANJG
+// BASE = INI BASE GW SENDIRI BUKAN PUNYA ORANG LU JAN SOTOY DEH
