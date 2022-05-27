9d6a0ccd36d946a8992817dacbb516cb7b587f88
BangsulBotz <99018264+BangsulBotz@users.noreply.github.com>
2022-05-27 21:27:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ audio14 = fs.readFileSync('./Z3/nayla14.m4a')
 audio15 = fs.readFileSync('./Z3/nayla15.m4a')
 audio16 = fs.readFileSync('./Z3/nayla16.m4a')
 audio17 = fs.readFileSync('./Z3/nayla17.m4a')
-audio18 = fs.readFileSync('./Z3/nayla18.m4a')
+audio18 = fs.readFileSync('./Z3/nayla23.m4a')
 audio19 = fs.readFileSync('./Z3/nayla22.m4a')
 audio20 = fs.readFileSync('./Z3/nayla21.m4a') 
             function kyun(seconds){
